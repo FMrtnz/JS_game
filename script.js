7c354de19938286d2659ab25bd2c8ab0fe2cff19
Fabien Martinez <fab.mrtnz22@gmail.com>
2017-06-09 18:01:21 +0200

    second tutorial moving & limited borders

--- script.js
+++ script.js
@@ -1,8 +1,67 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle ="#FF0000"
-ctx.fill();
-ctx.closePath();
+
+//second tutorial (moving)
+
+// setInterval(function, milliseconds);
+
+setInterval(draw, 10);
+
+var x = canvas.width/2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
+
+
+// Drawing code
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2, false);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  // Avoid repetition
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Drawing code
+  drawBall();
+
+  // Limite of borders code
+
+  if(x + dx < 0 || x + dx > canvas.width) {
+    dx = -dx;
+  }
+
+  if(y + dy < 0 || y + dy > canvas.height) {
+    dy = -dy;
+  }
+
+  // Moving parameters
+  x += dx;
+  y += dy;
+}
+
+// first tutorial
+
+// ctx.beginPath();
+// ctx.rect(20, 40, 50, 50);
+// ctx.fillStyle ="#FF0000";
+// ctx.fill();
+// ctx.closePath();
+
+// ctx.beginPath();
+// ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+// ctx.fillStyle = "green";
+// ctx.fill();
+// ctx.closePath();
+
+// ctx.beginPath();
+// ctx.rect(160, 10, 100, 40);
+// ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
+// ctx.stroke();
+// ctx.closePath();
